19622ae4c6217097c9d08fa2d0ffb51b80c1bf45
Erik <Erik@ERIK-PC>
2018-04-03 12:30:39 +0200

    Initial code setup (main disabled in matmul)

--- Tensorflow_Base/main.cpp
+++ Tensorflow_Base/main.cpp
@@ -13,7 +13,19 @@ using namespace std;
 
 int main()
 {
+	VideoCapture vid("data/ucf-101/BrushingTeeth/v_BrushingTeeth_g01_c01.avi");
 
+	if (!vid.isOpened()) cout << "Cannot open video file" << endl;
+
+	double count = vid.get(CV_CAP_PROP_FRAME_COUNT);
+	vid.set(CV_CAP_PROP_POS_FRAMES, count / 2);
+
+	Mat frame;
+	vid.read(frame);
+
+	namedWindow("test", CV_WINDOW_AUTOSIZE);
+	imshow("test", frame);
+	waitKey(0);
 
 	return 0;
 }
