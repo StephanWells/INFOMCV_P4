9992a9e5d0b6d1e993514df4ded374a37b0857ec
Erik <Erik@ERIK-PC>
2018-04-03 12:23:12 +0200

    Moved some files around

---
+++ Tensorflow_Base/main.cpp
@@ -0,0 +1,19 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/utility.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/calib3d.hpp"
+#include "opencv2/imgproc.hpp"
+
+#include <vector>
+#include <math.h>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+
+
+	return 0;
+}
