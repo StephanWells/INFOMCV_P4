c0a651d926959794e26f8271ba96d4f2fb0c5987
Erik <Erik@ERIK-PC>
2018-04-05 17:31:03 +0200

    Added initial TensorFlow integrations

---
+++ Tensorflow_Base/tfplus.h
@@ -0,0 +1,53 @@
+#pragma once
+#define COMPILER_MSVC
+#define NOMINMAX
+
+#include <utility>
+#include <tuple>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <stdexcept>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <math.h>
+#include <iostream>
+#include <algorithm>
+#include <random>
+#include <Windows.h>
+
+#include "tensorflow/cc/ops/const_op.h"
+#include "tensorflow/cc/ops/image_ops.h"
+#include "tensorflow/cc/ops/standard_ops.h"
+#include "tensorflow/core/framework/graph.pb.h"
+#include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/cc/framework/gradients.h"
+#include "tensorflow/core/graph/default_device.h"
+#include "tensorflow/core/graph/graph_def_builder.h"
+#include "tensorflow/core/lib/core/errors.h"
+#include "tensorflow/core/lib/core/stringpiece.h"
+#include "tensorflow/core/lib/core/threadpool.h"
+#include "tensorflow/core/lib/io/path.h"
+#include "tensorflow/core/lib/strings/stringprintf.h"
+#include "tensorflow/core/platform/init_main.h"
+#include "tensorflow/core/platform/logging.h"
+#include "tensorflow/core/platform/types.h"
+#include "tensorflow/core/public/session.h"
+#include "tensorflow/core/util/command_line_flags.h"
+#include "tensorflow/core/platform/load_library.h"
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/utility.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/calib3d.hpp"
+#include "opencv2/imgproc.hpp"
+
+using namespace tensorflow;
+using namespace tensorflow::ops;
+using namespace cv;
+using namespace std;
