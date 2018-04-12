78946ca9036086a7bc5dfcc44b3379add00bd67d
Erik <Erik@ERIK-PC>
2018-04-12 09:49:57 +0200

    Changes to LabelMat and Tensorflow setup

---
+++ Python/cnn.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 12 08:42:38 2018
+
+@author: Erik
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Imports
+import numpy as np
+import tensorflow as tf
+
+def cnn_model(features, labels, mode):
+    inputLayer = tf.reshape(features["x"], [-1, 90, 90, 3])
+    
+    conv1 = tf.layers.conv2d(
+            inputs=inputLayer,
+            filters=16,
+            kernel_size=[7, 7],
+            padding="valid",
+            activation=tf.nn.relu)
+    
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+    
+    conv2 = tf.layers.conv2d(
+            inputs=pool1,
+            filters=16,
+            kernel_size=[5, 5],
+            padding="valid",
+            activation=tf.nn.relu)
+    
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+    
+    flatten = tf.reshape(pool2, [-1, 19 * 19 * 16])
+    
+    dense = tf.layers.dense(inputs=flatten, units=1024, activation=tf.nn.relu)
+    
+    droplayer = tf.layers.dropout(
+            inputs=dense,
+            rate=0.4,
+            training=mode == tf.estimator.ModeKeys.TRAIN)
+    
+    logits = tf.layers.dense(inputs=droplayer, units=5)
+    
+    predictions = {
+     "classes": tf.argmax(input=logits, axis=1),
+     "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+    }
+    
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+    
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+    
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimiser = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+        train_op = optimiser.minimize(loss=loss, global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+    
+    eval_metric_ops = {
+        "accuracy": tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])
+    }
+    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
--- Python/main.py
+++ Python/main.py
@@ -15,19 +15,16 @@ import numpy as np
 import tensorflow as tf
 import cv2 as cv
 import os
+import cnn
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
 class Action(Enum):
-    BrushingTeeth = 1
-    CuttingInKitchen = 2
-    JumpingJack = 3
-    Lunges = 4
-    WallPushups = 5
-
-class LabelMat:
-    label = None
-    mat = None
+    BrushingTeeth = 0
+    CuttingInKitchen = 1
+    JumpingJack = 2
+    Lunges = 3
+    WallPushups = 4
 
 def getFrameMat(path):
     vid = cv.VideoCapture(path)
@@ -37,37 +34,55 @@ def getFrameMat(path):
     
     _, frame = vid.read()
     
-    cv.namedWindow('test', cv.WINDOW_AUTOSIZE)
-    cv.imshow('test', frame)
-    cv.waitKey(0)
+    #cv.namedWindow('test', cv.WINDOW_AUTOSIZE)
+    #cv.imshow('test', frame)
+    #cv.waitKey(0)
     
+    #print(str(np.size(frame, 0)) + ',' + str(np.size(frame, 1)) + os.linesep)
     vid.release()
-    cv.destroyAllWindows()
+    #cv.destroyAllWindows()
     return frame
 
 def getFrameMats():
     basepath = 'data/ucf-101/'
-    frames = []
+    labels = []
+    mats = []
     
+    print("Video loading...", end='')
     for action in Action:
         datapath = basepath + action.name + '/'
         
         for filename in os.listdir(datapath):
-            lm = LabelMat()
-            lm.label = action
+            labels.append(action.value)
             
             frame = getFrameMat(datapath + filename)
-            lm.mat = cv.resize(frame, (90, 90))
-            
-            print(lm.mat.size)
-            
-            frames.append(lm)
+            mat = cv.resize(frame, (90, 90))           
+            mats.append(np.float32(mat))
     
-    return frames
+    print("DONE.")
+    return mats, labels
 
 def main():
-    getFrameMats()
+    frame_mats, frame_labels = getFrameMats()
+    train_data = np.asarray(frame_mats)
+    train_labels = np.asarray(frame_labels)
     
+    classifier = tf.estimator.Estimator(model_fn=cnn.cnn_model, model_dir="/tmp/cnn_model")
+    
+    log_tensors = {"probabilities": "softmax_tensor"}
+    logging_hook = tf.train.LoggingTensorHook(tensors=log_tensors, every_n_iter=50)
+    
+    train_input = tf.estimator.inputs.numpy_input_fn(
+            x={"x": train_data},
+            y=train_labels,
+            batch_size=10,
+            num_epochs=None,
+            shuffle=True)
+    
+    classifier.train(
+            input_fn=train_input,
+            steps=20000,
+            hooks=[logging_hook])    
     
 if __name__ == "__main__":
     main()
